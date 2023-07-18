6aaa3b6cceca81c4f11037b7a51e2c4ac8484dbc
Imran Hosein Khan Joy <greatkhanjoy@gmail.com>
2023-07-18 15:02:31 +0600

    Mobile Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import { BsFillMoonFill, BsMoon } from "react-icons/bs";
-import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { BsFillMoonFill, BsMoon, BsGrid } from "react-icons/bs";
+import { AiOutlineHeart, AiFillHeart, AiOutlineClose } from "react-icons/ai";
 import logo from "./assets/img/logo.png";
 import logoWhite from "./assets/img/logo-white.png";
 import tree from "./assets/img/tree.png";
@@ -29,9 +29,14 @@ import footerTwo from "./assets/img/footer2.png";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
+  };
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
   };
 
   useEffect(() => {
@@ -44,7 +49,7 @@ const App = () => {
   return (
     <div className="dark:bg-darkBg">
       <BrowserRouter>
-        <header>
+        <header className="sticky top-0 z-40 bg-white dark:bg-darkBg">
           <div className="container">
             <div className="flex justify-between h-[72px] items-center">
               <div className="logo">
@@ -57,8 +62,8 @@ const App = () => {
                   />
                 </Link>
               </div>
-              <nav className="hidden md:flex">
-                <ul className="flex items-center gap-11 text-lg font-medium dark:text-white">
+              <nav className="flex gap-11">
+                <ul className="hidden md:flex items-center gap-11 text-lg font-medium dark:text-white">
                   <li className="transition-color duration-200 hover:text-red">
                     <Link to="/">Home</Link>
                   </li>
@@ -71,11 +76,52 @@ const App = () => {
                   <li className="transition-color duration-200 hover:text-red">
                     <Link to="/new">New</Link>
                   </li>
-                  <li onClick={toggleDarkMode} className="cursor-pointer">
+                </ul>
+                <div className="flex text-lg gap-4 dark:text-white">
+                  <button onClick={toggleDarkMode}>
                     {darkMode ? <BsMoon /> : <BsFillMoonFill />}
+                  </button>
+                  <button onClick={toggleMenu} className="text-lg md:hidden">
+                    <BsGrid />
+                  </button>
+                </div>
+              </nav>
+              {/* Mobile menu  */}
+              <div
+                className={`w-full fixed top-0 left-0 bg-white min-h-screen p-6 dark:bg-darkBg ${
+                  showMenu ? "slide-in" : "slide-out"
+                }`}
+              >
+                <ul className="text-lg flex flex-col gap-4 items-center dark:text-white">
+                  <li className="transition-color duration-200 hover:text-red">
+                    <Link to="/" className="hover:text-red">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="transition-color duration-200 hover:text-red">
+                    <Link to="/about" className="hover:text-red">
+                      Celebrate
+                    </Link>
+                  </li>
+                  <li className="transition-color duration-200 hover:text-red">
+                    <Link to="/gifts" className="hover:text-red">
+                      Gifts
+                    </Link>
+                  </li>
+                  <li className="transition-color duration-200 hover:text-red">
+                    <Link to="/new" className="hover:text-red">
+                      New
+                    </Link>
                   </li>
                 </ul>
-              </nav>
+                <button
+                  onClick={toggleMenu}
+                  className="text-lg absolute top-6 right-6 dark:text-white"
+                >
+                  <AiOutlineClose />
+                </button>
+              </div>
+              {/* End Mobile Menu  */}
             </div>
           </div>
         </header>
@@ -448,7 +494,7 @@ const App = () => {
             <img
               src={footerTwo}
               alt="footer-lamp"
-              className="absolute bottom-28 -right-14 md:right-0 "
+              className="absolute bottom-28 right-0 md:right-0 "
             />
           </div>
         </footer>
